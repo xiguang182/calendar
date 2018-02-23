5105aac617c6dc6a5d6c047a96f420da5ead4695
xiguang182 <xiguang182@gmail.com>
2018-02-23 17:13:31 -0500

    new element align on origin point

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const timeUnit = 48;
+var currentElement = null;
 // target elements with the "draggable" class
 interact('.draggable')
   .draggable({
     snap: {
       targets: [
         function (x, y) {
+          // let event = currentElement.getBoundingClientRect();
+          // x = event.left;
+          // y = event.top;
           let origin = document.getElementById("originPoint");
           let position = origin.getBoundingClientRect();
           let unitsX = Math.round((x - position.left)/ position.width);
@@ -12,6 +16,14 @@ interact('.draggable')
           let unitsY = Math.round((y - position.top)/ timeUnit);
           let snapY = position.top + unitsY * timeUnit;
           // console.log(position)
+          if(false){
+            //snap back
+            let startUnitsX = currentElement.getAttribute('unit-x') || 0;
+            let startUnitsY = currentElement.getAttribute('unit-y') || 0;
+            snapX = position.left + startUnitsX * position.width;
+            snapY = position.top + startUnitsY * timeUnit;
+          }
+          console.log(unitsX,unitsY,position,snapX, snapY)
           return {x: snapX, y: snapY, range: Infinity}
         },
       ],
@@ -29,7 +41,10 @@ interact('.draggable')
     },
     // enable autoScroll
     autoScroll: true,
-
+    onstart:function(event){
+      console.log(event.target)
+      currentElement = event.target;
+    },
     // call this function on every dragmove event
     onmove: dragMoveListener,
     // call this function on every dragend event
@@ -42,14 +57,45 @@ interact('.draggable')
       let unitsY = Math.round(y/ timeUnit);
       event.target.setAttribute('unit-x', unitsX);
       event.target.setAttribute('unit-y', unitsY);
+      currentElement = null;
     }
   });
 
+  function scrollListener(event){
+    if(currentElement == null){
+      
+      console.log("scrolled",event);
+      return;
+    } else {
+      console.log("scrolled",event.dy);
+    //   let target = currentElement,
+    //     // keep the dragged position in the data-x/data-y attributes
+    //     x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    //     y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    //     console.log(event.dx,event.dy);
+
+    // // translate the element
+    // target.style.webkitTransform =
+    // target.style.transform =
+    //   'translate(' + x + 'px, ' + y + 'px)';
+
+    // // update the posiion attributes
+    // target.setAttribute('data-x', x);
+    // target.setAttribute('data-y', y);
+    }
+  }
+  window.onload= ()=>{
+    console.log('load')
+    document.getElementById("eventScroll").addEventListener('scroll', scrollListener);
+  }
+  // window.addEventListener("scroll", scrollListener);
+
   function dragMoveListener (event) {
-    var target = event.target,
+    let target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        console.log(event.dx,event.dy);
 
     // translate the element
     target.style.webkitTransform =
@@ -67,8 +113,8 @@ interact('.draggable')
 interact('.timeColumn').dropzone({
   // only accept elements matching this CSS selector
   accept: '.draggable',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
+  // Require a 51% element overlap for a drop to be possible
+  overlap: 0.51,
 
   // listen for drop related events:
 
@@ -103,11 +149,11 @@ window.onresize = function(event) {
   let events = document.getElementsByClassName("event");
   let origin = document.getElementById("originPoint");
   let position = origin.getBoundingClientRect();
-  console.log(events)
+  // console.log(events)
   for(let i = 0; i< events.length; i++){
     let unitsX = events[i].getAttribute('unit-x') || 0;
     if(unitsX == 0){
-      return;
+      continue;
     }
     let y = parseFloat(events[i].getAttribute('data-y'));
     let x = unitsX * position.width;
@@ -124,3 +170,55 @@ window.onresize = function(event) {
     
   console.log(events);
 };
+
+
+document.addEventListener('keypress', (event) => {
+  if(event.code == 'Digit0'){
+    let newEvent = createEventNode();
+    alert('keypress event\n\n' + 'key: ' + event.code);
+  }
+});
+
+function createEventNode(){
+  let newEvent = document.createElement('div');
+  // let newContent = document.createTextNode("Hi there and greetings!"); 
+  newEvent.classList.add('draggable');
+  newEvent.classList.add('event');
+  let info = {
+    team:'team naaaaaaaaaaaaaaaaaaaaaaame',
+    time:'12:00pm - 01:30pm'
+  };
+  let team = document.createElement('span');
+  team.innerHTML = info.team;
+  newEvent.appendChild(team);
+  let time = document.createElement('span');
+  time.innerHTML = info.time;
+  newEvent.appendChild(time)
+  newEvent.setAttribute('info', '');
+  newEvent.innerHTML = 'team naaaaaaaaaaaaaaaaaaaaaaame<br>12:00pm-01:30pm';
+  // newEvent.appendChild(newContent);
+  let unitsX = 1;
+  let unitsY = 12;
+  let origin = document.getElementById("originPoint");
+  let position = origin.getBoundingClientRect();
+  // translate the element
+  let x = unitsX * position.width;
+  let y = unitsY * timeUnit;
+  newEvent.style.height = '71px';
+  newEvent.style.webkitTransform =
+  newEvent.style.transform =
+    'translate(' + x + 'px, ' + y + 'px)';
+
+  // update the posiion attributes
+  newEvent.setAttribute('data-x', x);
+  newEvent.setAttribute('data-y', y);
+  
+  newEvent.setAttribute('unit-x', unitsX);
+  newEvent.setAttribute('unit-y', unitsY);
+  origin.appendChild(newEvent);
+  return newEvent;
+}
+
+function overlap(unitX,unitY){
+
+}
